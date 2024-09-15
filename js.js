56e8f0af07995804d854c82b66a63a3e1f57af96
unknown <madaawadh1@gmail.com>
2024-09-15 14:38:27 +0300

    reponsive

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ async function chars() {
 const response=await fetch('https://rickandmortyapi.com/api/character');
 const chars=await response.json();
 
-chars.results.map((e)=>container.innerHTML+=` <div class="card" style="width: 10rem;">
+chars.results.map((e)=>container.innerHTML+=` <div class="card" style="width: 14.5rem;">
         <img class="card-img-top" src=${e.image} alt="Card image cap">
         <div class="card-body"><h5 class="card-title">${e.name}</h5> </div>
       </div>`);
